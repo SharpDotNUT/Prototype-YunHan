a545dd98c1a61d83f7ac437f8662765ebbca1769
Kuriyota <Kuriyota@outlook.com>
2025-03-09 16:19:37 +0800

    改进翻译；修改首页和关于页

---
+++ src/locales/reduce.runtime.ts
@@ -0,0 +1,15 @@
+// 扫描 JSON 文件，
+// 去除空对象
+// 按键排序
+
+const dir = Deno.readDirSync('.')
+dir.forEach(async (file) => {
+    if (file.name.endsWith('.json')) {
+        const data = JSON.parse(await Deno.readTextFile(file.name))
+        const sorted = Object.keys(data).sort().reduce((acc, key) => {
+            acc[key] = data[key]
+            return acc
+        }, {} as Record<string, any>)
+        await Deno.writeTextFile(file.name, JSON.stringify(sorted, null, 2))
+    }
+})
--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,33 +1,43 @@
 <template>
   <div class="__container_about">
     <div id="main">
-      <img src="/img/UI_NameCardPic_Yunjin_P.png" style="width: 100%; border-radius: 20px;opacity: 0.75;" />
+      <img
+        src="/img/UI_NameCardPic_Yunjin_P.png"
+        style="width: 100%; border-radius: 20px; opacity: 0.75" />
       <div id="main_text">
-        <p style="font-size: 5vw">{{ $t("name") }}</p>
+        <p style="font-size: 5vh">{{ $t('name') }}</p>
         <var-badge :value="'v ' + PackageJSON.version"></var-badge>
+        <br />
         <p>
-          <var-link href="https://github.com/SharpDotNUT" target="_blank"> #.NUT Studio </var-link>
-          |
-          <var-link href="https://github.com/Kuriyota" target="_blank">Kuriyota</var-link>
+          <span>
+            <span>By </span>
+            <var-link href="https://github.com/SharpDotNUT" target="_blank">
+              <img style="height: 1em" src="\img\Logo_FB.proj.svg" />
+              <span>&nbspStudio</span>
+            </var-link>
+          </span>
         </p>
       </div>
     </div>
-    <a :href="`https://github.com/${Meta.repo}`" target="_blank" style="display: flex; gap: 5px">
+    <a
+      :href="`https://github.com/${Meta.repo}`"
+      target="_blank"
+      style="display: flex; gap: 5px">
       <img :src="`https://img.shields.io/github/stars/${Meta.repo}`" />
       <img :src="`https://img.shields.io/github/license/${Meta.repo}`" />
     </a>
     <p>
-      {{ $t("about.build-time", [new Date(BuildTime).toLocaleString()]) }}
+      {{ $t('about.build-time', [new Date(BuildTime).toLocaleString()]) }}
     </p>
     <p>Copyright © 2025 SharpDotNUT. All rights reserved.</p>
   </div>
 </template>
 
 <script setup lang="ts">
-  import PackageJSON from "../../../package.json";
-  import Meta from "@/meta";
-  const BuildTime = __BUILD_TIME;
-  const show = defineModel("show", { type: Boolean, default: true });
+  import PackageJSON from '../../../package.json'
+  import Meta from '@/meta'
+  const BuildTime = __BUILD_TIME
+  const show = defineModel('show', { type: Boolean, default: true })
 </script>
 
 <style lang="css" scoped>

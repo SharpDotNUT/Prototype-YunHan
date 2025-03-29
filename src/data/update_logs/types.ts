dd6c818c50dc2375fbc10303cb089ff635d007d9
Kuriyota <Kuriyota@outlook.com>
2025-03-30 00:01:02 +0800

    [Update Log] New Feature

---
+++ src/data/update_logs/types.ts
@@ -0,0 +1,13 @@
+export type t_UpdateLog = Array<{
+  version: string
+  date: string
+  'name-zh': string
+  'name-en': string
+  items: Array<{
+    area: string
+    type: 'UI & UX' | 'Bug Fix' | 'Feature' | 'Performance' | 'Others'
+    'content-zh': string
+    'content-en': string
+  }>
+  commits?: Array<string>
+}>

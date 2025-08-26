5ce5cebff9271f4d00a3fe3a009c165bbfda20df
Kuriyota <Kuriyota@outlook.com>
2025-08-26 20:40:30 +0800

    style: update game font url

--- src/components/font-select/example-text.ts
+++ src/components/font-select/example-text.ts
@@ -1,16 +1,16 @@
 export const ExampleTextGI = {
   'zh-Hans': [
     '「云婵娟来花婵娟，风流尽在山水间。江月半轮佳人面，娥眉青酒动心田。」',
-    'GI-zh-cn'
+    'GI_zh-CN'
   ],
   en: [
     '"How gently do the clouds and flowers sway, as dancing winds meander through the vales. ' +
       'The half-moon is as a lovely face, and a glass raised to it moves the heart."',
-    'GI-zh-cn'
+    'GI_zh-CN'
   ],
   ja: [
     '「美しき雲に愛しき花、風流は山水にあり。川面に映る半月は美しき顔、細き眉と蒼き酒に心掴まれる」。',
-    'GI-ja-jp'
+    'GI_ja-JP'
   ]
 };
 export const ExampleTextHSR = {
@@ -18,38 +18,38 @@ export const ExampleTextHSR = {
     '手工雕琢的银戒，附耳能够听到雪融的声音。\n' +
       '在哀地里亚，逝者临行前将冰雪封入戒指，献给生者，代为同行余下生路。\n' +
       '「冰消雪释时，花会盛开，你会找到我。」',
-    'HSR-zh-cn'
+    'HSR_zh-CN'
   ],
   en: [
     'A handcrafted silver ring — if you hold it to your ear, you can hear the sound of melting snow.\n' +
       'In Aidonia, the departed seal ice and snow into a ring before their final journey, \n' +
       'offering it to the living as a token to walk the rest of the path in their stead\n' +
       '"When the ice melts and the snowfades, the flowers will bloom, and youwill find me."',
-    'HSR-zh-tw'
+    'HSR_zh-tw'
   ],
   ja: [
     '手作りの銀の指輪。耳を当てると雪解けの音が聞こえるようだ。\n' +
       'エイジリアでは、死者が旅立つ前に氷雪を指輪に封じ、生者に贈る習慣がある。残った者に道を示すためだそうだ。\n' +
       '「雪が解け、花が咲く時、あなたは私を見つけられるでしょう」',
-    'HSR-ja-jp'
+    'HSR_ja-JP'
   ]
 };
 export const ExampleTextZZZ = {
   'zh-Hans': [
     '阵营：对空洞特别行动部第六课；身高：170（包括耳朵）\n' +
       '经历的战斗数量越多，越是能让外人察觉到一个事实：若是有刀剑的神灵存在，那他们一定颇为偏爱星见雅。',
-    'ZZZ-zh-cn'
+    'ZZZ_zh-CN'
   ],
   en: [
     'Faction: Hollow Special Operations Section 6; Height: 170 (Ears Included)\n' +
       'The more she battles, the more it becomes evident to outsiders:\n' +
       'If there is a god of swordsmanship, it must greatly favor Hoshimi Miyabi.',
-    'ZZZ-en-us'
+    'ZZZ_en-US'
   ],
   ja: [
     '対ホロウ特別行動部第六課；身長：170（耳を含む）\n' +
       '幾度にもわたる戦いの果てに、人々は確信を持つようになった——\n' +
       'もしこの世に刀剣の神が存在するのなら、それは星見雅を寵愛しているに違いない、と。',
-    'ZZZ-ja-jp'
+    'ZZZ_ja-JP'
   ]
 };

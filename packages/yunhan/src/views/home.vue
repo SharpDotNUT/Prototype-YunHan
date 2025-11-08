b178853ff1e448599b3fc3b2c3471ec0a5cfccc2
Kuriyota <Kuriyota@outlook.com>
2025-11-08 23:35:28 +0800

    style(home): add background image
    Introduces a full-screen background image to the home page and restructures the layout by wrapping main content in a <main> element. Updates CSS for improved positioning, styling, and visual hierarchy.

--- packages/yunhan/src/views/home.vue
+++ packages/yunhan/src/views/home.vue
@@ -7,22 +7,25 @@ import { NavigationData } from '@/nav';
 
 <template>
   <div class="home-container">
-    <!--TEXT ID: 3225099370, 2148997250-->
-    <p v-html="$t('home.welcome', [$t('global.username.traveler')])"></p>
-    <nav>
-      <router-link v-for="item in NavigationData" :to="item.path">
-        <var-card class="card" ripple>
-          <h3>{{ $t(item.name + '.title') }}</h3>
-          <p>
-            {{ $t(item.name + '.description') }}
-            <br />
-            <var-chip v-if="item.beta" size="small" type="warning">
-              Beta
-            </var-chip>
-          </p>
-        </var-card>
-      </router-link>
-    </nav>
+    <img
+      src="https://yunhan-data.sharpdotnut.com/Static/Genshin-UI/UI_Download_Bg.png" />
+    <main>
+      <p v-html="$t('home.welcome', [$t('global.username.traveler')])"></p>
+      <nav>
+        <router-link v-for="item in NavigationData" :to="item.path">
+          <var-card class="card" ripple>
+            <h3>{{ $t(item.name + '.title') }}</h3>
+            <p>
+              {{ $t(item.name + '.description') }}
+              <br />
+              <var-chip v-if="item.beta" size="small" type="warning">
+                Beta
+              </var-chip>
+            </p>
+          </var-card>
+        </router-link>
+      </nav>
+    </main>
   </div>
 </template>
 

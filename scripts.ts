c13e18ab4a8f60180ea2a55b879facb245e2f798
Kuriyota <Kuriyota@outlook.com>
2025-11-09 16:39:22 +0800

    Release 2.8.1

---
+++ scripts.ts
@@ -0,0 +1,28 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+const dirname = import.meta.dirname as string;
+const filename = import.meta.filename as string;
+
+const updateVersion = async () => {
+  const files = [
+    './package.json',
+    './packages/yunhan/package.json',
+    './packages/meta/package.json'
+  ];
+  const version = process.argv[3];
+  for (const file of files) {
+    try {
+      const data = await fs.readFile(file, 'utf-8');
+      const json = JSON.parse(data);
+      json.version = version;
+      await fs.writeFile(file, JSON.stringify(json, null, 2));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
+switch (process.argv[2]) {
+  case 'version':
+    updateVersion();
+}

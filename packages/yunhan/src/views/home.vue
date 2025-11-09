d3516af71d005d8ed0b80d057c6cf28beb7a21cd
Kuriyota <Kuriyota@outlook.com>
2025-11-09 15:51:45 +0800

    style(home): redesign layout and styles
    Refactored home page structure by introducing #bg and #content containers, updating CSS for improved layout and responsiveness, and enhancing the main section with cards and a welcome image. These changes improve visual hierarchy and user experience.

--- packages/yunhan/src/views/home.vue
+++ packages/yunhan/src/views/home.vue
@@ -26,33 +26,49 @@ ky.get('https://bing.biturl.top/').then(async (wal) => {
 
 <template>
   <div class="home-container">
-    <img :src="enableWallpaper && wallpaper ? wallpaper?.url : GenshinBG" />
-    <main>
-      <p v-html="$t('home.welcome', [$t('global.username.traveler')])"></p>
-      <nav>
-        <router-link v-for="item in NavigationData" :to="item.path">
-          <var-card class="card" ripple>
-            <h3>{{ $t(item.name + '.title') }}</h3>
-            <p>
-              {{ $t(item.name + '.description') }}
-              <br />
-              <var-chip v-if="item.beta" size="small" type="warning">
-                Beta
-              </var-chip>
-            </p>
-          </var-card>
-        </router-link>
-      </nav>
-      <div
-        style="
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-        ">
-        <p>{{ $t('home.enable-bw') }}</p>
-        <var-switch v-model="enableWallpaper" />
-      </div>
-    </main>
+    <img
+      id="bg"
+      :src="enableWallpaper && wallpaper ? wallpaper?.url : GenshinBG" />
+    <div id="content">
+      <main>
+        <var-card>
+          <h1>{{ $t('home.title') }}</h1>
+          <br />
+          <div class="flex" style="gap: 10px">
+            <img
+              height="72"
+              src="https://yunhan-data.sharpdotnut.com/Static/Genshin-UI/EmotionIcon/UI_EmotionIcon181.png" />
+            <p
+              v-html="$t('home.welcome', [$t('global.username.traveler')])"></p>
+          </div>
+        </var-card>
+        <nav>
+          <router-link v-for="item in NavigationData" :to="item.path">
+            <var-card class="card" ripple>
+              <h3>{{ $t(item.name + '.title') }}</h3>
+              <p>
+                {{ $t(item.name + '.description') }}
+                <br />
+                <var-chip v-if="item.beta" size="small" type="warning">
+                  Beta
+                </var-chip>
+              </p>
+            </var-card>
+          </router-link>
+        </nav>
+        <var-card>
+          <div
+            style="
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+            ">
+            <p>{{ $t('home.enable-bw') }}</p>
+            <var-switch v-model="enableWallpaper" />
+          </div>
+        </var-card>
+      </main>
+    </div>
   </div>
 </template>
 

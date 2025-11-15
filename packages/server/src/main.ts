1426117e5762ce6f7b917b91768db285791ea6a7
Kuriyota <Kuriyota@outlook.com>
2025-11-15 15:55:38 +0800

    add package `@prototype-yunhan/server`

---
+++ packages/server/src/main.ts
@@ -0,0 +1,18 @@
+import { Elysia } from 'elysia';
+import { RKeys } from './router/key.ts';
+import { cors } from '@elysiajs/cors';
+
+const app = new Elysia();
+
+app.get('/', () => 'Hello TruE Elysia!');
+
+app.use(
+  cors({
+    origin: ['https://yunhan.sharpdotnut.com', /^http:\/\/localhost:\d+$/]
+  })
+);
+app.use(RKeys);
+
+app.listen(52100);
+
+console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
---
+++ packages/server/src/router/key.ts
@@ -0,0 +1,59 @@
+import { Elysia, t } from 'elysia';
+import { Database } from 'bun:sqlite';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+const dirname = import.meta.dirname as string;
+const filename = import.meta.filename as string;
+
+const db = new Database(path.join(dirname, '../../db/keys.sqlite3'));
+
+const app = new Elysia({
+  prefix: '/sp-keys'
+});
+export { app as RKeys };
+
+app.get('/all', () => {
+  const data = db.query('SELECT * FROM keys').all();
+  return data;
+});
+app.post(
+  '/add',
+  async (ctx) => {
+    try {
+      if (!ctx.body) {
+        ctx.set.status = 400;
+        return 'No body';
+      }
+      const {
+        token,
+        key,
+        game,
+        version,
+        server,
+        'available-time': availableTime
+      } = ctx.body;
+      if (token != 'YunJinYYT0521') {
+        throw new Error('Unauthorized');
+      }
+      const data = db
+        .query(
+          'INSERT INTO keys (key, game, server, version, "available-time") VALUES (?, ?, ?, ?, ?)'
+        )
+        .all(key, game, server, version, availableTime);
+      return data;
+    } catch (err) {
+      ctx.set.status = 500;
+      return err;
+    }
+  },
+  {
+    body: t.Object({
+      token: t.String(),
+      key: t.String(),
+      game: t.String(),
+      version: t.String(),
+      server: t.String(),
+      'available-time': t.Number()
+    })
+  }
+);
